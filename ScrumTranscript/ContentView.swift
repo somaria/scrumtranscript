2cb5ee89d7461ee3ec3b4b155baa79ad01f56900
Daryl <daryl.9@gmail.com>
2023-03-23 12:33:10 +0800

    Initial Commit

---
+++ ScrumTranscript/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ScrumTranscript
+//
+//  Created by admin on 23/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrumTranscript/ScrumTranscriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumTranscriptApp.swift
+//  ScrumTranscript
+//
+//  Created by admin on 23/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumTranscriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
